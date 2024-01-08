5ea119b1f05e86007c839cd3a3e98030ee722392
Anton-U01 <toni.urdjanov@abv.bg>
2024-01-08 19:01:08 +0200

    Data definition and Datatypes Lab

---
+++ Data Definition and Datatypes Lab/2. Insert Data in Tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name) values ('Ivan', 'Ivanov');
+INSERT INTO employees (first_name, last_name) values ('Goergi', 'Georgiev');
+INSERT INTO employees (first_name, last_name) values ('Mitko', 'Mitkov');
